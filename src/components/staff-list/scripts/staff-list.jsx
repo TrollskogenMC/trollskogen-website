31ed1cde6dcba55799ad6977ff7658e3b2ba9c54
Philip Koivunen <philip.koivunen@outlook.com>
2019-04-25 18:41:59 +0200

    added player heads for vixaan and mistresslilith

--- src/components/staff-list/scripts/staff-list.jsx
+++ src/components/staff-list/scripts/staff-list.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../styles/staff.css";
 import philip from "../images/philip2096.png";
 import hornta from "../images/hornta.png";
+import vixaan from "../images/vixaan.png";
+import mistresslilith from "../images/mistresslilith.png";
 
 export default function StaffList({ text }) {
   return (
@@ -15,6 +17,16 @@ export default function StaffList({ text }) {
         <img src={philip} width="75" height="75" alt="philip2096" />
         Philip2096
         <br /> Admin
+      </div>
+      <div className="staff-member">
+        <img src={vixaan} width="75" height="75" alt="vixaan" />
+        vixaan
+        <br /> Moderator
+      </div>
+      <div className="staff-member">
+        <img src={mistresslilith} width="75" height="75" alt="mistresslilith" />
+        mistresslilith
+        <br /> Moderator
       </div>
     </div>
   );
