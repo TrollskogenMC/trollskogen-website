77d29c6b47eae036775dd97d9be3162bc6390ada
Philip Koivunen <philip.koivunen@outlook.com>
2019-04-25 20:15:35 +0200

    fixed a styling issue where some staff members would be hidden on smaller screensizes

--- src/components/staff-list/scripts/staff-list.jsx
+++ src/components/staff-list/scripts/staff-list.jsx
@@ -9,22 +9,46 @@ export default function StaffList({ text }) {
   return (
     <div className="staff-list">
       <div className="staff-member">
-        <img src={hornta} width="75" height="75" alt="hornta" />
+        <img
+          className="staff-img"
+          src={hornta}
+          width="75px"
+          height="75px"
+          alt="hornta"
+        />
         hornta
         <br /> Admin
       </div>
       <div className="staff-member">
-        <img src={philip} width="75" height="75" alt="philip2096" />
+        <img
+          className="staff-img"
+          src={philip}
+          width="75px"
+          height="75px"
+          alt="philip2096"
+        />
         Philip2096
         <br /> Admin
       </div>
       <div className="staff-member">
-        <img src={vixaan} width="75" height="75" alt="vixaan" />
+        <img
+          className="staff-img"
+          src={vixaan}
+          width="75px"
+          height="75px"
+          alt="vixaan"
+        />
         vixaan
         <br /> Moderator
       </div>
       <div className="staff-member">
-        <img src={mistresslilith} width="75" height="75" alt="mistresslilith" />
+        <img
+          className="staff-img"
+          src={mistresslilith}
+          width="75px"
+          height="75px"
+          alt="mistresslilith"
+        />
         mistresslilith
         <br /> Moderator
       </div>
