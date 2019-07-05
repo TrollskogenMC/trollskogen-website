4bb229b56efbc534ae10a091a9968a65a2776d6c
Philip Koivunen <philip.koivunen@outlook.com>
2019-07-05 23:49:41 +0200

    removed raceing from navbar

--- src/pages/racing/scripts/racing.jsx
+++ src/pages/racing/scripts/racing.jsx
@@ -3,22 +3,7 @@ import axios from "axios";
 export default function Racing() {
   const getRaces = () => {
     axios
-      .get("https://api.trollskogen.nu/races", {
-        crossdomain: true,
-        "Content-Type": "application/json"
-      })
-      .then(response => {
-        console.log(response);
-        return "success";
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    axios
-      .get("https://api.trollskogen.nu/users", {
-        crossdomain: true,
-        "Content-Type": "application/json"
-      })
+      .get("http://api.trollskogen.nu/users", { crossdomain: true })
       .then(response => {
         console.log(response);
         return "success";
