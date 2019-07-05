e9364c7abdb47edbbeee9ebf45395cc4010bad35
Philip Koivunen <philip.koivunen@outlook.com>
2019-07-06 00:33:44 +0200

    test push

--- src/pages/racing/scripts/racing.jsx
+++ src/pages/racing/scripts/racing.jsx
@@ -3,7 +3,22 @@ import axios from "axios";
 export default function Racing() {
   const getRaces = () => {
     axios
-      .get("http://api.trollskogen.nu/users", { crossdomain: true })
+      .get("https://api.trollskogen.nu/races", {
+        crossdomain: true,
+        "Content-Type": "application/json"
+      })
+      .then(response => {
+        console.log(response);
+        return "success";
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    axios
+      .get("https://api.trollskogen.nu/users", {
+        crossdomain: true,
+        "Content-Type": "application/json"
+      })
       .then(response => {
         console.log(response);
         return "success";
