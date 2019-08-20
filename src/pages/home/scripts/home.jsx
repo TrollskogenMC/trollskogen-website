28cfad1e7d8ed00f8b76f052bb47a1cbfa3e551a
Philip Koivunen <philip.koivunen@outlook.com>
2019-08-20 19:10:53 +0200

    removed some irrelevant text for startpage and modified racing page and added new rule

--- src/pages/home/scripts/home.jsx
+++ src/pages/home/scripts/home.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
     <div>
       <Jumbotron
         className="jumbotron-bg-home"
-        title="Trollskogen - Ny svensk minecraft server"
+        title="Trollskogen - Svensk minecraft server 1.14"
         text="Kul att du kikar in! Trollskogen är ett nytt svenskt
     minecraft-community som startades i februari 2019. Vi värnar om att
     ha ett moget community och därför kommer vi att vara stränga med
@@ -19,23 +19,6 @@ export default function HomePage() {
         }
       />
 
-      <div className="post-area">
-        <div className="post post-left">
-          <hr />
-          <h2 className="post-title">Nu är det dags att uppdatera till 1.14!</h2>
-          <p className="post-text">
-            Med uppdateringen så kommer ett gängg nya funktioner till servern,
-            förutom alla nya vannilla funktionerna så har vi passat på att
-            uppdatera mcMMO, lagt till vårt nya plugin "racing".
-            <br />
-            <br />
-            För att fira att 1.14 äntligen har kommit till Trollskogen så slår
-            vi på <b>dubbel xp-event </b>
-            för mcMMO från tisdag den 6e augusti till och med söndag 11e augusti
-            (23.59).
-          </p>
-        </div>
-      </div>
       <div className="post-area">
         <br />
         <div className="post post-left">
