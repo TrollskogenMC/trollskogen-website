f146b49032036bd7b28331053a1c01f10bf3d50b
Philip Koivunen <philip.koivunen@outlook.com>
2019-11-20 23:02:01 +0100

    started pocing carousel

--- src/pages/home/scripts/home.jsx
+++ src/pages/home/scripts/home.jsx
@@ -8,6 +8,7 @@ export default function HomePage() {
   useEffect(() => {
     changeImageList(
       shuffle([
+<<<<<<< HEAD
         "jumbotron-bg-anim1",
         "jumbotron-bg-anim3",
         "jumbotron-bg-anim4",
@@ -28,12 +29,26 @@ export default function HomePage() {
     return () => clearInterval(interval);
   });
 
+=======
+        "jumbotron-bg-home",
+        "jumbotron-bg-donate",
+        "jumbotron-bg-racing"
+      ])
+    );
+    setInterval(changeImageId, 5000);
+  }, []);
+
+>>>>>>> started pocing carousel
   const changeImageList = list => {
     setImageList(list);
   };
 
   const changeImageId = () => {
+<<<<<<< HEAD
     if (imageList.length - 1 === currentImageId) setCurrentImageId(0);
+=======
+    if (!currentImageId) setCurrentImageId(0);
+>>>>>>> started pocing carousel
     else setCurrentImageId(currentImageId + 1);
   };
 
@@ -51,9 +66,14 @@ export default function HomePage() {
   return (
     <div>
       <Jumbotron
+<<<<<<< HEAD
         className={`${imageList[currentImageId] ||
           "jumbotron-bg-anim1"} jumbotron-animate`}
         title="Trollskogen - Svensk minecraft server 1.15"
+=======
+        className={imageList[currentImageId]}
+        title="Trollskogen - Svensk minecraft server 1.14"
+>>>>>>> started pocing carousel
         text="Kul att du kikar in! Trollskogen är ett nytt svenskt
     minecraft-community som startades i februari 2019. Vi värnar om att
     ha ett moget community och därför kommer vi att vara stränga med
