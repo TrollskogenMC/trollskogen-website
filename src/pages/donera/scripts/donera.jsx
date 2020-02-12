509d583b0aa95c8a7d4c5642be3600d19101d603
Philip Koivunen <philip.koivunen@outlook.com>
2020-02-12 19:17:47 +0100

    stopped using vipplus donationlist and made some styling corrections

--- src/pages/donera/scripts/donera.jsx
+++ src/pages/donera/scripts/donera.jsx
@@ -7,6 +7,59 @@ import Jumbotron from "../../../components/jumbotron/scripts/jumbotron.jsx";
 import "../styles/donera.css";
 
 export default function Donera() {
+  const vipPlusContent = () => (
+    <div className="donate-content">
+      <InfoBoxContainer>
+        <InfoBoxHeader className="text-center info-box-header-purple">
+          <div className="donate-info-text">VIP+ förmåner</div>
+        </InfoBoxHeader>
+        <InfoBoxInfo>
+          <div className="group-list">
+            <ul className="fc-light-grey">
+              <li className="padding color-gold">
+                /ci - Rensar ditt inventory
+              </li>
+              <li className="padding color-gold">
+                /hat - Ha ett block/item som hatt
+              </li>
+              <li className="padding color-gold">
+                /tpa /tpahere - Teleportera dig till en vän eller den till dig
+              </li>
+              <li className="padding color-gold">
+                /top - Tar dig till den högsta platsen ovanför dig
+              </li>
+              <li className="padding color-gold">
+                /tpa /tpahere - Teleportera dig till en vän eller den till dig
+              </li>
+              <li className="padding color-gold">/trail - Kosmetisk effekt.</li>
+              <li className="padding color-purple">
+                /feed - fyll på din hunger (40 min cooldown)
+              </li>
+              <li className="padding color-purple">
+                /enderchest - öppna en enderchest utan att ha en i ditt
+                inventory
+              </li>
+              <li className="padding color-purple">
+                /getpos - få koordinater till en spelare som är inne på servern
+              </li>
+              <li className="padding color-purple">
+                /near - lista spelare i närheten
+              </li>
+              <li className="padding color-purple">
+                /seen - se hur länge sedan en spelare var inne på servern
+              </li>
+              <li className="padding">
+                Du får också 4 extra hem med /sethome, ett lila namn i vår
+                discord-server och i chatten och du kan sälja 8 saker istället
+                för 2 i /ah.
+              </li>
+            </ul>
+          </div>
+        </InfoBoxInfo>
+      </InfoBoxContainer>
+    </div>
+  );
+
   return (
     <div>
       <Jumbotron
@@ -46,61 +99,6 @@ export default function Donera() {
                   <li className="padding">
                     Du får också 2 extra hem med /sethome, ett guldigt namn i
                     vår discord-server och i chatten och du kan sälja 5 saker
-                    istället för 2 i /ah.
-                  </li>
-                </ul>
-              </div>
-            </InfoBoxInfo>
-          </InfoBoxContainer>
-        </div>
-        <div className="donate-content">
-          <InfoBoxContainer>
-            <InfoBoxHeader className="text-center info-box-header-purple">
-              <div className="donate-info-text">VIP+ förmåner</div>
-            </InfoBoxHeader>
-            <InfoBoxInfo>
-              <div className="group-list">
-                <ul className="fc-light-grey">
-                  <li className="padding color-gold">
-                    /ci - Rensar ditt inventory
-                  </li>
-                  <li className="padding color-gold">
-                    /hat - Ha ett block/item som hatt
-                  </li>
-                  <li className="padding color-gold">
-                    /tpa /tpahere - Teleportera dig till en vän eller den till
-                    dig
-                  </li>
-                  <li className="padding color-gold">
-                    /top - Tar dig till den högsta platsen ovanför dig
-                  </li>
-                  <li className="padding color-gold">
-                    /tpa /tpahere - Teleportera dig till en vän eller den till
-                    dig
-                  </li>
-                  <li className="padding color-gold">
-                    /trail - Kosmetisk effekt.
-                  </li>
-                  <li className="padding color-purple">
-                    /feed - fyll på din hunger (40 min cooldown)
-                  </li>
-                  <li className="padding color-purple">
-                    /enderchest - öppna en enderchest utan att ha en i ditt
-                    inventory
-                  </li>
-                  <li className="padding color-purple">
-                    /getpos - få koordinater till en spelare som är inne på
-                    servern
-                  </li>
-                  <li className="padding color-purple">
-                    /near - lista spelare i närheten
-                  </li>
-                  <li className="padding color-purple">
-                    /seen - se hur länge sedan en spelare var inne på servern
-                  </li>
-                  <li className="padding">
-                    Du får också 4 extra hem med /sethome, ett lila namn i vår
-                    discord-server och i chatten och du kan sälja 8 saker
                     istället för 2 i /ah.
                   </li>
                 </ul>
