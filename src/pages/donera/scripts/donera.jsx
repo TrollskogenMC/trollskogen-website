a09934bd701c55d693ebfedb71b2bbeac72b1f52
Philip Koivunen <philip.koivunen@outlook.com>
2020-03-21 21:13:29 +0100

    readded vipplus and added discord section to rules page

--- src/pages/donera/scripts/donera.jsx
+++ src/pages/donera/scripts/donera.jsx
@@ -8,56 +8,48 @@ import "../styles/donera.css";
 
 export default function Donera() {
   const vipPlusContent = () => (
-    <div className="donate-content">
-      <InfoBoxContainer>
-        <InfoBoxHeader className="text-center info-box-header-purple">
-          <div className="donate-info-text">VIP+ förmåner</div>
-        </InfoBoxHeader>
-        <InfoBoxInfo>
-          <div className="group-list">
-            <ul className="fc-light-grey">
-              <li className="padding color-gold">
-                /ci - Rensar ditt inventory
-              </li>
-              <li className="padding color-gold">
-                /hat - Ha ett block/item som hatt
-              </li>
-              <li className="padding color-gold">
-                /tpa /tpahere - Teleportera dig till en vän eller den till dig
-              </li>
-              <li className="padding color-gold">
-                /top - Tar dig till den högsta platsen ovanför dig
-              </li>
-              <li className="padding color-gold">
-                /tpa /tpahere - Teleportera dig till en vän eller den till dig
-              </li>
-              <li className="padding color-gold">/trail - Kosmetisk effekt.</li>
-              <li className="padding color-purple">
-                /feed - fyll på din hunger (40 min cooldown)
-              </li>
-              <li className="padding color-purple">
-                /enderchest - öppna en enderchest utan att ha en i ditt
-                inventory
-              </li>
-              <li className="padding color-purple">
-                /getpos - få koordinater till en spelare som är inne på servern
-              </li>
-              <li className="padding color-purple">
-                /near - lista spelare i närheten
-              </li>
-              <li className="padding color-purple">
-                /seen - se hur länge sedan en spelare var inne på servern
-              </li>
-              <li className="padding">
-                Du får också 4 extra hem med /sethome, ett lila namn i vår
-                discord-server och i chatten och du kan sälja 8 saker istället
-                för 2 i /ah.
-              </li>
-            </ul>
-          </div>
-        </InfoBoxInfo>
-      </InfoBoxContainer>
-    </div>
+    <InfoBoxContainer>
+      <InfoBoxHeader className="text-center info-box-header-purple">
+        <div className="donate-info-text">VIP+ förmåner</div>
+      </InfoBoxHeader>
+      <InfoBoxInfo>
+        <div className="group-list">
+          <ul className="fc-light-grey">
+            <li className="padding color-gold">/ci - Rensar ditt inventory</li>
+            <li className="padding color-gold">
+              /hat - Ha ett block/item som hatt
+            </li>
+            <li className="padding color-gold">
+              /tpa /tpahere - Teleportera dig till en vän eller den till dig
+            </li>
+            <li className="padding color-gold">
+              /top - Tar dig till den högsta platsen ovanför dig
+            </li>
+            <li className="padding color-gold">
+              /tpa /tpahere - Teleportera dig till en vän eller den till dig
+            </li>
+            <li className="padding color-gold">/trail - Kosmetisk effekt.</li>
+            <li className="padding color-purple">
+              /enderchest - öppna en enderchest utan att ha en i ditt inventory
+            </li>
+            <li className="padding color-purple">
+              /getpos - få koordinater till en spelare som är inne på servern
+            </li>
+            <li className="padding color-purple">
+              /near - lista spelare i närheten
+            </li>
+            <li className="padding color-purple">
+              /seen - se hur länge sedan en spelare var inne på servern
+            </li>
+            <li className="padding">
+              Du får också 4 extra hem med /sethome, ett lila namn i vår
+              discord-server och i chatten och du kan sälja 8 saker istället för
+              2 i /ah.
+            </li>
+          </ul>
+        </div>
+      </InfoBoxInfo>
+    </InfoBoxContainer>
   );
 
   return (
@@ -78,6 +70,7 @@ export default function Donera() {
         }
       />
       <div className="donations-list">
+        <div className="donate-content">{vipPlusContent()}</div>
         <div className="donate-content">
           <InfoBoxContainer>
             <InfoBoxHeader className="text-center info-box-header-gold">
