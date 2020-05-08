474c01affcac03e39a1ad08a34750cd449ea7c55
Philip Koivunen <philip.koivunen@outlook.com>
2020-05-08 23:04:13 +0200

    fixed some manifest issues and changed site description

--- src/pages/home/scripts/home.jsx
+++ src/pages/home/scripts/home.jsx
@@ -54,10 +54,9 @@ export default function HomePage() {
         className={`${imageList[currentImageId] ||
           "jumbotron-bg-anim1"} jumbotron-animate`}
         title="Trollskogen - Svensk minecraft server 1.15"
-        text="Kul att du kikar in! Trollskogen är ett nytt svenskt
-    minecraft-community som startades i februari 2019. Vi värnar om att
-    ha ett moget community och därför kommer vi att vara stränga med
-    reglerna."
+        text="Kul att du kikar in! Trollskogen är ett svenskt
+    minecraft-community som startades i februari 2019. Vi kör med ett gäng plugin, så som towny, mcmmo, lwc m.fl.
+    och vi värnar om att ha ett moget community."
         linkArea={
           <React.Fragment>
             <span>Ip: mc.trollskogen.nu</span>
