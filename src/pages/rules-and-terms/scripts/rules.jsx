855d1857ff74d8404a5b41cc72f2bf1f16879cfe
Philip Koivunen <philip.koivunen@outlook.com>
2020-09-06 16:33:29 +0200

    added new rule for towny tax

--- src/pages/rules-and-terms/scripts/rules.jsx
+++ src/pages/rules-and-terms/scripts/rules.jsx
@@ -90,6 +90,10 @@ export default function Rules() {
           <li>
             Tillsvidare är det inte okej att göra farmer för pillagers/illagers.
           </li>
+          <li>
+            Om man har en speciell taxa för sin stad måste man förmedla tydligt
+            vad den taxan innebär för alla medborgare.
+          </li>
         </ol>
       </ul>
 
